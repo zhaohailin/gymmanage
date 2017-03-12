0fd85f55e05ef231976d08ca34faa20d6761c762
zhaohailin <18288314@qq.com>
2017-03-12 13:07:58 +0800

    Signed-off-by: zhaohailin <18288314@qq.com>3.12

---
+++ webbuilding/www/Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,39 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html lang="zh-CN">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>
+			WebBuilding_前台主页
+		</title>
+		<!-- Bootstrap CSS -->
+		<import type="css" file="Css.Common.bootstrap.min">
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+		
+	<import type="css" href="Css.Home.index">
+
+		<!-- jQuery -->
+		<import file="Js.Common.jquery.min"></script>
+		<!-- Bootstrap JavaScript -->
+		<import file="Js.Common.bootstrap.min"></script>
+		<!-- jquery版本兼容 -->
+		<import file="Js.Common.jquery-migrate-1.1.0"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+		<import file="Js.Common.ie10-viewport-bug-workaround"></script>
+	</head>
+	<body>
+		
+	
+	webbuilding
+
+	</body>
+	
+	<import file="Js.Home.index"></script>
+
+</html>
